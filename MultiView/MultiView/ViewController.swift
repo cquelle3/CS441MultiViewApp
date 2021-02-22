480c7eb515de997aefaf59f25a2e86d3bcc5d469
Colin Quelle <cquelle3@binghamton.edu>
2021-02-22 09:55:45 -0500

    Created xcode project

---
+++ MultiView/MultiView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiView
+//
+//  Created by Colin Quelle on 2/22/21.
+//  Copyright © 2021 Colin Quelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
