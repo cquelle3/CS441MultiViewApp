0dbaa173305430a2f77e7ff95f15ff347c0fee08
Colin Quelle <cquelle3@binghamton.edu>
2021-02-22 10:41:53 -0500

    Setting scene of view navigation game. Created multiple views/transitions

---
+++ MultiView/MultiView/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  MultiView
+//
+//  Created by Colin Quelle on 2/22/21.
+//  Copyright © 2021 Colin Quelle. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
