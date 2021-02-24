3640b0b04277e17c6760c0dbf756b7487f885acf
Colin Quelle <cquelle3@binghamton.edu>
2021-02-24 10:41:15 -0500

    Started writing event management script

---
+++ MultiView/MultiView/EventManagement.swift
@@ -0,0 +1,18 @@
+//
+//  EventManagement.swift
+//  MultiView
+//
+//  Created by Colin Quelle on 2/24/21.
+//  Copyright © 2021 Colin Quelle. All rights reserved.
+//
+
+import Foundation
+
+class EventManagement{
+    
+    var pickUpSword = false;
+    var pickUpKey = false;
+    
+    
+    
+}
